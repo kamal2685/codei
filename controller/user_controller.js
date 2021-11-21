7e85c4e2b35ad899e8f726161463cdb0f1dfd5db
Kamal mall <Kamal>
2021-11-21 17:02:24 +0530

    adding ejs and post route and controller

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(req,res){
     return res.send('<h1> PROFILE LOADED </h1>');
 }
+module.exports.posts = function(req,res){
+    return res.send('POSTs!!');
+}
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const controller = require('../controller/user_controller');
 
 router.get('/profile', controller.profile);
+router.get('/posts',controller.posts);
 
 module.exports = router;

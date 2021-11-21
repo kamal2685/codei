7e1a6669b61809bcbbf0d896aa4ed9e7ef960724
Kamal mall <Kamal>
2021-11-21 23:31:38 +0530

    applying different views to diff controllers

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.send("Hello! from controller home");
+    return res.render('home',{
+        title : "home"
+    })
 }
--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req,res){
-    return res.send('<h1> PROFILE LOADED </h1>');
+    return res.render('user',{
+        title : "user"
+    });
 }
 module.exports.posts = function(req,res){
     return res.send('POSTs!!');
